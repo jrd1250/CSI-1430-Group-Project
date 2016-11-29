db4f23907c2b008b86b39425b5512ebb6cf5d23d
jrd1250 <jrd1250@gmail.com>
2016-11-29 13:31:54 -0600

    Add files via upload

---
+++ Draw.cpp
@@ -0,0 +1,160 @@
+/*
+Author: J.R. Diehl
+Title: Draw Library Implementation File
+*/
+
+#include "Draw.h"
+#include <stdlib.h>
+#include <time.h>
+
+void Draw(int x, int y, SDL_Plotter& g, std::ifstream& in,
+          std::string toDraw, bool flip)
+{
+    int row, col;
+    int R,G,B;
+
+    in.open(toDraw.c_str());
+
+    if(!in.is_open())
+    {}
+    else
+    {
+      in >> row >> col;
+
+      if(flip)
+      {
+        for(int i = 0; i < row; i++)
+        {
+          for(int j = col; j > 0; j--)
+          {
+            in >> R >> G >> B;
+            if(R == 255 && G == 255 && B == 255)
+            {}
+            else
+            {
+              g.plotPixel(x + j, y + i, R, G, B);
+            }
+          }
+        }
+      }
+      else
+      {
+        for(int i = 0; i < row; i++)
+        {
+          for(int j = 0; j < col; j++)
+          {
+            in >> R >> G >> B;
+            if(R == 255 && G == 255 && B == 255)
+            {}
+            else
+            {
+              g.plotPixel(x + j, y + i, R, G, B);
+            }
+          }
+        }
+      }
+      in.close();
+    }
+}
+
+void modDraw(int x, int y, SDL_Plotter& g, std::ifstream& in,
+          std::string toDraw, std::string modColor, bool flip)
+{
+    int row, col;
+    int R,G,B;
+    int lR, lG, lB, dR, dG, dB;   //modified values for R, G, and B in light
+                                  //and dark
+
+    if(modColor == "Fire")
+    {
+      dR = 207;
+      dG = 16;
+      dB = 26;
+      lR = 227;
+      lG = 132;
+      lB = 36;
+    }
+    else if(modColor == "Water")
+    {
+      dR = 19;
+      dG = 31;
+      dB = 249;
+      lR = 126;
+      lG = 219;
+      lB = 226;
+    }
+    else if(modColor == "Earth")
+    {
+      dR = 113;
+      dG = 92;
+      dB = 23;
+      lR = 114;
+      lG = 109;
+      lB = 92;
+    }
+    else if(modColor == "Air")
+    {
+      dR = 86;
+      dG = 218;
+      dB = 48;
+      lR = 237;
+      lG = 237;
+      lB = 107;
+    }
+    else
+    {
+      dR = dG = dB = 0;
+      lR = lG = lB = 127;
+    }
+
+    in.open(toDraw.c_str());
+    if(!in.is_open())
+    {}
+    else
+    {
+      in >> row >> col;
+
+      if(flip)
+      {
+        for(int i = 0; i < row; i++)
+        {
+          for(int j = col; j > 0; j--)
+          {
+            in >> R >> G >> B;
+            if(R == 255 && G == 255 && B == 255)
+            {}
+            else if(R == 127 && G == 127 && B == 127)
+            {
+              g.plotPixel(x + j, y + i, lR, lG, lB);
+            }
+            else if(R == 0 && G == 0 && B == 0)
+            {
+              g.plotPixel(x + j, y + i, dR, dG, dB);
+            }
+          }
+        }
+      }
+      else
+      {
+        for(int i = 0; i < row; i++)
+        {
+          for(int j = 0; j < col; j++)
+          {
+            in >> R >> G >> B;
+            if(R == 255 && G == 255 && B == 255)
+            {}
+            else if(R == 127 && G == 127 && B == 127)
+            {
+              g.plotPixel(x + j, y + i, lR, lG, lB);
+            }
+            else if(R == 0 && G == 0 && B == 0)
+            {
+              g.plotPixel(x + j, y + i, dR, dG, dB);
+            }
+          }
+        }
+      }
+
+      in.close();
+    }
+}
--- Game.h
+++ Game.h
@@ -3,19 +3,41 @@ Author: J.R.Diehl
 Title: Header file for game functions library
 */
 
-#include "Plotter.h"
 #include "Pokemon.h"
+#include "Plotter.h"
+#include "Draw.h"
+#include "Player.h"
+#include <iostream>
 #include <fstream>
 
 #ifndef GAME_H
 #define GAME_H
 
-void Draw(int x, int y, SDL_Plotter& g, ifstream& in);
+Pokemon GenPoke(std::string heads[], std::string bodies[], std::string arms[],
+             std::string legs[], std::string types[], std::string prefix[],
+             std::string name1[], std::string name2[], std::string name3[],
+             std::string name4[], int headDefenseMods[], int bodyHPMods[],
+             int armDamageMods[], int legDamageMods[], int headNum,
+             int bodyNum, int armNum, int legNum, int typeNum);
 
-void Start();
-
-void Move(int x, int y, SDL_Plotter& g, ifstream& in);
-
-void Battle(Pokemon active, Pokemon enemy)
+Pokemon GenFromID(int index, std::string heads[], std::string bodies[],
+                std::string arms[],
+                std::string legs[], std::string types[], std::string prefix[],
+                std::string name1[], std::string name2[], std::string name3[],
+                std::string name4[], int headDefenseMods[], int bodyHPMods[],
+                int armDamageMods[], int legDamageMods[], int headNum,
+                int bodyNum, int armNum, int legNum, int typeNum);
+/*
+Player Start(SDL_Plotter& g, std::ifstream& in,
+             std::string heads[], std::string bodies[],
+             std::string arms[],
+             std::string legs[], std::string types[], std::string prefix[],
+             std::string name1[], std::string name2[], std::string name3[],
+             std::string name4[], int headDefenseMods[], int bodyHPMods[],
+             int armDamageMods[], int legDamageMods[], int headNum,
+             int bodyNum, int armNum, int legNum, int typeNum);
+*/
+void Battle(Pokemon& active, Pokemon& enemy, SDL_Plotter& g,
+            std::ifstream& in, std::ostream& out, int playerX, int playerY);
 
 #endif // GAME_H
